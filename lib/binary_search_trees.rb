bf7ba48783c5566a93a95bc1a69353c53cbf09aa
Stefano Rondelli <rstefanofil@hotmail.com>
2022-04-10 11:16:51 +0200

    Add #balanced? method

--- lib/binary_search_trees.rb
+++ lib/binary_search_trees.rb
@@ -254,6 +254,27 @@ class Tree
         end
     end
 
+    def balanced?
+        height_diff_array = Array.new
+        nodes = self.breadth_first
+        balanced_inspection(nodes, height_diff_array)
+        height_diff_array.all? { | element | [-1, 0, 1].include?(element) }
+    end
+
+    def balanced_inspection(nodes, height_diff_array)
+        nodes.each do | node |
+            unless node.left_child.nil? && node.right_child.nil?
+                if node.left_child.nil?
+                    height_diff_array.push(height(node.data))
+                elsif node.right_child.nil?
+                    height_diff_array.push(height(node.data))
+                else
+                    height_diff_array.push(height(node.left_child.data.to_i) - height(node.right_child.data.to_i))
+                end
+            end
+        end
+    end
+
     def pretty_print(node = @root, prefix = '', is_left = true)
         pretty_print(node.right_child, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right_child
         puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
@@ -274,14 +295,14 @@ end
 
 array = [1, 4, 8, 12, 32, 56, 78, 97, 121, 135, 245, 321, 654, 786, 981]
 tree = Tree.new(array)
-double = Proc.new { | node |
-    node.map! do | element |
-        element.data *= 2
-    end
-    puts node.join(", ")
-}
-tree.pretty_print
-tree.level_order(&double)
+# double = Proc.new { | node |
+#     node.map! do | element |
+#         element.data *= 2
+#     end
+#     puts node.join(", ")
+# }
+# tree.pretty_print
+# tree.level_order(&double)
 # tree.level_order
 # tree.pretty_print
 # puts tree.height(12)
@@ -289,6 +310,7 @@ tree.level_order(&double)
 # tree.insert(30)
 # tree.pretty_print
 # puts tree.height(12)
+
 
 
 # array = (Array.new(15) { rand(1..100) }).uniq!
